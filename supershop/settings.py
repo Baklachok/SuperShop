79b6ea62b1e4ad4de2cae24b9e75b6b59b303450
Skvoznyak3 <grisha_grigorev_02@mail.ru>
2024-07-04 00:24:55 +0300

    added hosts

--- supershop/settings.py
+++ supershop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-i06dz1w)4^dj06dlf!r_cb91g&e7dv*ixb(cq$syn*)ji&o_z-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend']
 
 
 # Application definition
