90c8795bc57537be0524c67523f2c1b0671c73ce
Baklachok <korobko_01@inbox.ru>
2024-07-06 16:45:15 +0300

    completed item model

--- api/admin.py
+++ api/admin.py
@@ -5,4 +5,5 @@ from api.models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price')
+    list_display = ('name', 'description', 'price', 'category', 'rating', 'order_count', 'discount', 'price_with_discount')
+    readonly_fields = ('price_with_discount',)
--- api/models.py
+++ api/models.py
@@ -1,31 +1,42 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-class MyUser(AbstractUser):
-    groups = models.ManyToManyField(
-        Group,
-        related_name='myuser_set',  # Измените related_name
-        blank=True,
-        help_text='The groups this user belongs to.',
-        verbose_name='groups'
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='myuser_set',  # Измените related_name
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions'
-    )
-    name = models.CharField(max_length=100)
-    telNo = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    passwordConfirmation = models.CharField(max_length=100)
+# class MyUser(AbstractUser):
+#     groups = models.ManyToManyField(
+#         Group,
+#         related_name='myuser_set',  # Измените related_name
+#         blank=True,
+#         help_text='The groups this user belongs to.',
+#         verbose_name='groups'
+#     )
+#     user_permissions = models.ManyToManyField(
+#         Permission,
+#         related_name='myuser_set',  # Измените related_name
+#         blank=True,
+#         help_text='Specific permissions for this user.',
+#         verbose_name='user permissions'
+#     )
+#     name = models.CharField(max_length=100)
+#     telNo = models.CharField(max_length=100)
+#     password = models.CharField(max_length=100)
+#     passwordConfirmation = models.CharField(max_length=100)
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], default=0)
+    category = models.CharField(max_length=100, null=True)
+    #category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
+    order_count = models.PositiveIntegerField(default=0)
+    discount = models.DecimalField(max_digits=3, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(1)], default=0)
+    price_with_discount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], default=0, editable=False)
+    # general_photo = models.ForeignKey(Item_Photos, related_name='items', limit_choices_to={'is_gen': True})
+
+    def save(self, *args, **kwargs):
+        self.price_with_discount = self.price * (1 - self.discount)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
