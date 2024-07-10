b60de86fb7dae2a857d0c4f8b5437e99c73e7901
Darkmen203 <125760866+Darkmen203@users.noreply.github.com>
2024-07-11 00:27:55 +0300

    fix api when page nothing found

--- api/pagination.py
+++ api/pagination.py
@@ -5,3 +5,27 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size_query_param = 'limit'
     max_page_size = 100
     page_query_param = 'page'
+    def paginate_queryset(self, queryset, request, view=None):
+        # Пытаемся получить объект страницы
+        try:
+            self.page = self.paginate_queryset(queryset)
+        except pagination.PageNotAnInteger:
+            self.page = self.page.paginator.page(1)
+        except pagination.EmptyPage:
+            self.page = []
+        
+        if not self.page:
+            return None
+        
+        self.request = request
+        return list(self.page)
+    
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })
