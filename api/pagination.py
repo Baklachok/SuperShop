012fdacb87a9fdd9790bd43cbad4e33650f1d029
Skvoznyak3 <grisha_grigorev_02@mail.ru>
2024-07-11 01:59:49 +0300

    pretty output for pagination

--- api/pagination.py
+++ api/pagination.py
@@ -1,31 +1,42 @@
-from rest_framework import pagination
+from django.core.paginator import PageNotAnInteger, EmptyPage, InvalidPage
+from django.http import JsonResponse
+from rest_framework import pagination, status
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+
 
 class CustomPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'limit'
     max_page_size = 100
     page_query_param = 'page'
+
     def paginate_queryset(self, queryset, request, view=None):
-        # Пытаемся получить объект страницы
-        try:
-            self.page = self.paginate_queryset(queryset)
-        except pagination.PageNotAnInteger:
-            self.page = self.page.paginator.page(1)
-        except pagination.EmptyPage:
-            self.page = []
-        
-        if not self.page:
-            return None
-        
+        """
+        Paginate a queryset if required, either returning a
+        page object, or `None` if pagination is not configured for this view.
+        """
         self.request = request
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = self.get_page_number(request, paginator)
+
+        try:
+            self.page = paginator.page(page_number)
+        except InvalidPage as exc:
+            msg = self.invalid_page_message.format(
+                page_number=page_number, message=str(exc)
+            )
+            return
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+
         return list(self.page)
-    
+
     def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
-            },
-            'count': self.page.paginator.count,
-            'results': data
-        })
+        return Response(data)
