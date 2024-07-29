d0d6266996ac78cc357206a5fbf0a0d2c8e494b4
Skvoznyak3 <grisha_grigorev_02@mail.ru>
2024-07-30 00:50:45 +0300

    +sms +forgot passs +sms activate acc

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-
 from django.contrib.auth import authenticate
+from django.core import cache
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from authentication.models import TelNoCode
 from supershop.settings import SIMPLE_JWT
+from supershop.smser import send_verification_code
 from supershop.telephone_check import isMobile
 from .models import FrontendUser, UserRefreshToken
 
@@ -42,6 +44,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
             telNo=validated_data['telNo'],
             password=validated_data['password'],
             name=validated_data['name'],
+            is_active=False,
         )
         return user
 
@@ -49,10 +52,12 @@ class RegistrationSerializer(serializers.ModelSerializer):
 class MyTokenObtainSerializer(serializers.Serializer):
     telNo = serializers.CharField()
     password = serializers.CharField()
+    code = serializers.IntegerField(required=False, allow_null=True)
 
     def validate(self, attrs):
         data = super().validate(attrs)
         telNo = attrs.get('telNo')
+        code = attrs.get('code')
         password = attrs.get('password')
         if not telNo:
             raise ValidationError({"telNo": "Must include 'telNo'."})
@@ -61,6 +66,28 @@ class MyTokenObtainSerializer(serializers.Serializer):
         user = authenticate(username=telNo, password=password)
         if not user:
             raise ValidationError({"telNo": "Invalid phone number."})
+        if not user.is_active:
+            print('user account is disabled')
+            if code:
+                print('code est')
+                stored_object = TelNoCode.objects.get(telNo=telNo)
+                stored_code = stored_object.code
+                print(stored_code)
+                print(code)
+                if stored_code == code:
+                    print('code match')
+                    user.is_active = True
+                    user.is_phone_verified = True
+                    user.save()
+                    stored_object.delete()
+                    print("Ваш номер подтвержден!")
+                else:
+                    print("Неверный код.")
+            else:
+                print('nachali otpravku')
+                send_verification_code(telNo)
+                print('zakonchili otpravku')
+                raise ValidationError({"code": "User account is disabled."})
         refresh = RefreshToken.for_user(user)
         UserRefreshToken.objects.create(
             user=user,
@@ -71,3 +98,63 @@ class MyTokenObtainSerializer(serializers.Serializer):
         data['access'] = str(refresh.access_token)
         return data
 
+
+class ResetPasswordSerializer(serializers.Serializer):
+    telNo = serializers.CharField()
+    code = serializers.IntegerField(required=False, allow_null=True)
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        telNo = attrs.get('telNo')
+        code = attrs.get('code')
+        try:
+            user = FrontendUser.objects.get(telNo=telNo)
+        except FrontendUser.DoesNotExist:
+            raise ValidationError({"telNo": "User doesn't exist."})
+
+        if user.is_active:
+            if code:
+                print('code est')
+                try:
+                    stored_object = TelNoCode.objects.get(telNo=telNo)
+                    stored_code = stored_object.code
+                except TelNoCode.DoesNotExist:
+                    raise ValidationError({"code": "error"})
+                print(stored_code)
+                print(code)
+                if stored_code == code:
+                    print('code match')
+                    print("mpzhno perehodit k novomu!")
+                    stored_object.delete()
+                else:
+                    print("Неверный код.")
+                    raise ValidationError({"code": "doesnt match"})
+                refresh = RefreshToken.for_user(user)
+                UserRefreshToken.objects.create(
+                    user=user,
+                    token=str(refresh),
+                    expires_at=datetime.now() + SIMPLE_JWT['REFRESH_TOKEN_LIFETIME']
+
+                )
+                data['refresh'] = str(refresh)
+                data['access'] = str(refresh.access_token)
+                return data
+            else:
+                print('nachali otpravku')
+                send_verification_code(telNo)
+                print('zakonchili otpravku')
+                raise ValidationError({"code": "Haven't code"})
+        else:
+            raise ValidationError({"code": "User account is disabled."})
+
+
+
+class NewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField()
+    password_confirm = serializers.CharField()
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise ValidationError({'password': "Passwords do not match.",
+                                   'password_confirm': "Passwords do not match."})
+        return data
